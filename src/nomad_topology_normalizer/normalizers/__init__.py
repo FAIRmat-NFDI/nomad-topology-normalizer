f28099cba7db830df3e244a988bc8abaab4cbe07
Amir Golparvar <golparvar.amir@gmail.com>
2025-10-07 11:34:11 +0200

    added a placeholder for new data schema

--- src/nomad_topology_normalizer/normalizers/__init__.py
+++ src/nomad_topology_normalizer/normalizers/__init__.py
@@ -1,13 +1,13 @@
 from nomad.config.models.plugins import NormalizerEntryPoint
 
 
-class SystemNormalizerEntryPoint(NormalizerEntryPoint):
+class TopologyNormalizerEntryPoint(NormalizerEntryPoint):
     normalizer_level = 3
 
     def load(self):
-        from nomad_topology_normalizer.normalizers.normalizer import SystemNormalizer
+        from nomad_topology_normalizer.normalizers.normalizer import TopologyNormalizer
 
-        return SystemNormalizer(**self.dict())
+        return TopologyNormalizer(**self.dict())
 
 
 system_normalizer_plugin = SystemNormalizerEntryPoint(
