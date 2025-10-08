bda95f89b2e7344d73100ee2c11d558dd2619629
Amir Golparvar <golparvar.amir@gmail.com>
2025-10-08 16:30:47 +0200

    removed tests

--- tests/normalizers/test_normalizer.py
+++ tests/normalizers/test_normalizer.py
@@ -1,12 +1,57 @@
-from nomad.client import normalize_all
-from nomad.datamodel import EntryArchive, EntryMetadata
-from nomad.datamodel.metainfo.workflow import Workflow
+from nomad.datamodel.datamodel import EntryArchive
+from nomad.datamodel.results import Relation, System
+
+from src.nomad_topology_normalizer.normalizers.normalizer import (
+    add_system,
+    add_system_info,
+    get_topology_id,
+    get_topology_original,
+)
 
 
-def test_normalizer():
-    entry_archive = EntryArchive(
-        metadata=EntryMetadata(), workflow2=Workflow(name='test')
+def test_get_topology_id_formats_correctly():
+    assert get_topology_id(0) == 'results/material/topology/0'
+    assert get_topology_id(17) == 'results/material/topology/17'
+
+
+def test_get_topology_original_minimal_archive():
+    archive = EntryArchive()
+    original = get_topology_original(atoms=None, archive=archive)
+
+    assert isinstance(original, System)
+    assert original.method == 'parser'
+    assert original.label == 'original'
+    assert original.system_relation == Relation(type='root')
+    assert original.dimensionality is None
+
+
+def test_add_system_creates_parent_child_links_and_ids():
+    topo = {}
+
+    root = System(method='parser', label='root', system_relation=Relation(type='root'))
+    add_system(root, topo)
+    assert root.system_id == 'results/material/topology/0'
+    assert topo[root.system_id] is root
+    assert root.child_systems in (None, [])
+
+    child1 = System(
+        method='parser', label='child1', system_relation=Relation(type='subsystem')
     )
+    add_system(child1, topo, parent=root)
+    assert child1.parent_system == root.system_id
+    assert root.child_systems and child1.system_id in root.child_systems
 
-    normalize_all(entry_archive)
-    assert entry_archive.workflow2.name == 'test'
+    child2 = System(
+        method='parser', label='child2', system_relation=Relation(type='subsystem')
+    )
+    add_system(child2, topo, parent=root)
+    assert len(root.child_systems) == 2
+    assert {child1.system_id, child2.system_id} == set(root.child_systems)
+
+
+def test_add_system_info_is_safe_without_atoms():
+    topo = {}
+    sys = System(method='parser', label='no_atoms')
+    add_system(sys, topo)
+    # Should be a no-op, but must not crash
+    add_system_info(sys, topo, masses=None)
