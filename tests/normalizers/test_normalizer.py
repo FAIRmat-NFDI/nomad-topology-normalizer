e68626672ab546a4b13bb300e1434b5651f7e48c
Bernadette Mohr <bernadette.mohr@physik.hu-berlin.de>
2025-10-08 15:12:28 +0200

    starting update

--- tests/normalizers/test_normalizer.py
+++ tests/normalizers/test_normalizer.py
@@ -1,12 +1,54 @@
-from nomad.datamodel.datamodel import EntryArchive
+import pytest
+from nomad.datamodel import EntryArchive, EntryMetadata
 from nomad.datamodel.results import Relation, System
 
+# from nomad.normalizing.topology import TopologyNormalizer
+from nomad.utils import get_logger
+
+from nomad_topology_normalizer.normalizers.normalizer import TopologyNormalizer
 from src.nomad_topology_normalizer.normalizers.normalizer import (
     add_system,
     add_system_info,
     get_topology_id,
     get_topology_original,
 )
+
+LOGGER = get_logger(__name__)
+
+
+@pytest.fixture
+def mock_normalizer(
+    entry_archive_with_simulation, mock_repr_system, mock_repr_symmetry, mock_logger
+):
+    """Create a TopologyNormalizer."""
+
+    normalizer = TopologyNormalizer(
+        entry_archive=entry_archive_with_simulation,
+        repr_system=mock_repr_system,
+        repr_symmetry=mock_repr_symmetry,
+        conv_atoms=None,
+        logger=mock_logger,
+    )
+
+    return normalizer
+
+
+def test_topology_calculation_2():
+    archive = EntryArchive(metadata=EntryMetadata())
+
+    class MockSystem:
+        atoms = None
+
+    normalizer = TopologyNormalizer(
+        entry_archive=archive,
+        repr_system=MockSystem(),
+        repr_symmetry=None,
+        conv_atoms=None,
+        logger=LOGGER,
+    )
+
+    result = normalizer.topology_calculation_2()
+    assert result is not None
 
 
 def test_get_topology_id_formats_correctly():
